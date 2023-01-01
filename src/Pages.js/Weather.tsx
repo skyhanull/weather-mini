fc604aafac36a9e281f61d875bfafb28f1de3505
skyhanull <anky0425@naver.com>
2023-01-01 16:01:06 +0900

    docs:문서수정

--- src/Pages.js/Weather.tsx
+++ src/Pages.js/Weather.tsx
@@ -1,115 +1,78 @@
 import styled from "styled-components";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useState } from "react";
 import "./Weather.css";
-import * as firebase from "firebase/app";
+import { getWeather } from "../Store/Slices/CurrentApi";
 import React from "react";
 import WeatherCard from "./WeaderCard";
-
-const Webresult = styled.div`
-  margin-top: 20px;
-  background-color: rgba(214, 255, 251, 0.2);
-  border-radius: 20px;
-  width: 300px;
-  height: 300px;
-`;
-
-const CardContent = styled.div`
-  /* width: calc(90vw / 20%); */
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  height: 50vh;
-  /* height: 10%;
-  background-color: red; */
-`;
-interface TimeA {
-  weather: any;
-}
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../Store/Store";
+import { useAppSelect } from "../Store/Store";
+import { editCityName } from "../Store/Slices/CityName";
+import { WeatherCurrentApi } from "../Store/Sagas/CurrentThunk";
+import { addList } from "../Store/Slices/ListSlice";
 
 const Weather = () => {
-  const Apikey = "c020c3284c152b3c7b93993db86d6d7f";
-  const [cityname, SetCityname] = useState("");
-  const [lat, setLat] = useState("");
-  const [sum, setSum] = useState([]);
-  const [lon, setLon] = useState("");
-  const [result, Setresult] = useState<any>([]);
-  const [currentData, setCurrentData] = useState<any>([]);
-  const [res1, setRes1] = useState<any>([]);
+  // const WeatherFiveList = useSelector(
+  //   (state: RootState) => state.WeatherFiveDay
+  // );
+  // const CityNameInput = useSelector(
+  //   (state: RootState) => state.useCityNameSlice
+  // );
+  const List = useSelector((state: RootState) => state.ListItem.item);
+  const Wea = useAppSelect(getWeather);
+  const WeaData = Wea.data;
 
-  // const As = (dt_txt: any) => {
-  //   if (new Date(dt_txt).getDate() === new Date().getDate()) {
-  //     // res1.push(new Date(dt_txt).getDate());
-  //     res1.push(new Date(dt_txt).getDate());
-  //   }
-  // };
+  const dispatch = useDispatch<AppDispatch>();
+  const [text, setText] = useState("");
 
-  // function onGeoOkay(position: any) {
-  //   setLat(position.coords.latitude);
-  //   setLon(position.coords.longitude);
-  //   // 위도 경도 변수 선언
-  //   // console.log("You live in", lat, lon);
-  // }
-
-  // function onGeoError() {
-  //   alert("I can't find you. No weather for you.");
-  // }
-
-  // navigator.geolocation.getCurrentPosition(onGeoOkay, onGeoError);
-
-  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityname}&appid=${Apikey}`;
-  const url1 = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${Apikey}`;
-
-  const weatherHandler = async () => {
-    const res = await axios({
-      method: "get",
-      url: url,
-    });
-    const { data } = res;
-    Setresult(data.list);
+  const changeName = (value: string) => {
+    dispatch(editCityName(value));
+    dispatch(WeatherCurrentApi(value));
+    // dispatch(WeatherFiveApi(value));
+    dispatch(addList(WeaData));
   };
 
-  const CityHandler = (e: any) => {
-    SetCityname(e.target.value);
+  const ClickHandler = (e: string) => {
+    changeName(e);
+    setText("");
   };
-
-  const currentWeather = async () => {
-    const res = await axios({
-      method: "get",
-      url: url1,
-    });
-    const { data } = res;
-    console.log(data);
-    setCurrentData(data);
-  };
-  // console.log(result.list);
-  // result.map((el: any, index: any) => {
-  //   if (new Date(el.dt_txt).getDate !== new Date().getDate) {
-  //     setSum(el.dt_txt);
-  //     console.log(el.dt_txt);
-  //   }
-  // });
 
   return (
-    <div className="input-section">
-      <div className="searchbar">
-        <input
-          className="input-box"
-          type="text"
-          placeholder="Search"
-          value={cityname}
-          onChange={CityHandler}
-        />
-      </div>
-      <button onClick={weatherHandler}> aaa</button>
-      <CardContent>
-        {result &&
-          result.map((el: any) => (
-            <WeatherCard key={el.dt} dt_txt={el.dt_txt} weather={el.weather} />
+    <>
+      <input
+        value={text}
+        type="text"
+        onChange={(e) => setText(e.target.value)}
+      ></input>
+      <button onClick={() => ClickHandler(text)}>A</button>
+      {/* <div>{CurrentList.weather[0]}</div> */}
+      {/* <div>
+        {CurrentList &&
+          Object.values(CurrentList).map((el: any, index: number) => (
+            <WeatherCard
+              key={index}
+              // Temp={el.main.temp}
+              // main={el[index].weather}
+              Text={el.name}
+              data={el}
+            />
           ))}
-        {/* <WeatherCard result={result}></WeatherCard> */}
-      </CardContent>
-    </div>
+      </div> */}
+      {/* <WeatherCard
+        // Temp={CurrentList?.main?.temp}
+        main={CurrentList.weather}
+        Text={CurrentList.name}
+      /> */}
+      {/* <WeatherCard WeaData={WeaData} List={List} /> */}
+      {List.map((el: any, index: any) => (
+        <WeatherCard
+          key={index}
+          id={el?.W?.id}
+          weather={el?.W?.weather[0].main}
+          temp={el?.W?.main.temp}
+        ></WeatherCard>
+      ))}
+    </>
   );
 };
 
