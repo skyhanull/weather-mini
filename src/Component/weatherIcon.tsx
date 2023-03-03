f2e5a19224912746ac3f869e028fd9bcb6084269
skyhanull <anky0425@naver.com>
2023-03-03 18:38:46 +0900

    feat: 날씨에 따라 아이콘을 나오게하는 컴포넌트

---
+++ src/Component/weatherIcon.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Icon } from '@iconify/react';
+
+const WeatherIcon = (weather: string) => {
+  return (
+    <div>
+      {weather === 'Clouds' ? (
+        <Icon icon="ic:round-cloud-queue" width="100" height="100" />
+      ) : weather === 'Clear' ? (
+        <Icon icon="ph:sun" width="100" height="100" />
+      ) : weather === 'Snow' ? (
+        <Icon icon="bi:cloud-snow" width="100" height="100" />
+      ) : weather === 'Rain' ? (
+        <Icon icon="carbon:rain" width="100" height="100" />
+      ) : null}
+    </div>
+  );
+};
+
+export default WeatherIcon;
