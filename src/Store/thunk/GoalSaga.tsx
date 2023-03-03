00946c97234be886b9fd94e3ad44a8c08583b9cc
skyhanull <anky0425@naver.com>
2023-03-03 18:49:58 +0900

    feat: thunk생성

---
+++ src/Store/thunk/GoalSaga.tsx
@@ -0,0 +1,19 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const ApiM = process.env.REACT_APP_API_KEY;
+
+export const WeatherFiveApi = createAsyncThunk(
+  "WeatherFiveApi",
+  async (value: string) => {
+    try {
+      const res = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${value}&appid=` +
+          ApiM
+      );
+      return await res.data;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+);
