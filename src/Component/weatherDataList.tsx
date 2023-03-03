3fadcef3a86dbafc046371124b5f681f1206f685
skyhanull <anky0425@naver.com>
2023-03-03 18:49:16 +0900

    feat: 공통컴포넌트 생성

---
+++ src/Component/weatherDataList.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+interface IweatherData {
+  name: string;
+  weather?: string;
+  temp: number;
+}
+const WeatherDataList = ({ name, weather, temp }: IweatherData) => {
+  return (
+    <div>
+      <div>{name}</div>
+      <div>{weather}</div>
+      <div>{(temp - 273.15).toFixed(2)}</div>
+    </div>
+  );
+};
+
+export default WeatherDataList;
