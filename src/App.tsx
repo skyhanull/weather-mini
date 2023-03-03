33c0b60bbaf9adba8003575848c7f34fff95c155
skyhanull <anky0425@naver.com>
2023-03-03 23:52:33 +0900

    add:필요없는 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import Weather from './Pages.js/Weather';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import "./App.css";
-
-// import { createGlobalStyle } from "styled-components";
 import React from 'react';
 import WeatherSub from './Pages.js/WeatherSub';
-// import About from './Pages.js/About';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="section-two">
-        <section>
-          <Routes>
-            <Route path="/" element={<Weather />} />
-            <Route path="/sub" element={<WeatherSub />} />
-            <Route path="/detail" element={<WeatherSub />} />
-            {/* <Route path="/about" element={<About />} /> */}
-          </Routes>
-        </section>
-      </div>
+      <Routes>
+        <Route path="/" element={<Weather />} />
+        <Route path="/sub" element={<WeatherSub />} />
+        <Route path="/detail/:name" element={<WeatherSub />} />
+      </Routes>
     </BrowserRouter>
   );
 }
