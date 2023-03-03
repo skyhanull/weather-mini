ebae928484868ee3855574f7bbe282d429b485d8
skyhanull <anky0425@naver.com>
2023-03-03 18:50:13 +0900

    add: 주석삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
-import Weather from "./Pages.js/Weather";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
-import SideBar from "./Pages.js/SideBar";
-import { createGlobalStyle } from "styled-components";
-import React from "react";
-import WeatherSub from "./Pages.js/WeatherSub";
-// import Dust from './Pages.js/Dust';
+import Weather from './Pages.js/Weather';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import "./App.css";
 
-const GlobalStyle = createGlobalStyle`
-body{
-  background: url("https://cdn.pixabay.com/photo/2020/05/06/06/18/blue-5136251_1280.jpg") ;
-  background-size: cover;
-
-
-}
-`;
+// import { createGlobalStyle } from "styled-components";
+import React from 'react';
+import WeatherSub from './Pages.js/WeatherSub';
+// import About from './Pages.js/About';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="section-two">
-        <GlobalStyle />
-        <main>
-          <SideBar />
-        </main>
         <section>
           <Routes>
             <Route path="/" element={<Weather />} />
             <Route path="/sub" element={<WeatherSub />} />
-            {/* <Route path='/dustinfo' element={<Dust />}/> */}
+            <Route path="/detail" element={<WeatherSub />} />
+            {/* <Route path="/about" element={<About />} /> */}
           </Routes>
         </section>
       </div>
